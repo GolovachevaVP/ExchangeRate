2e172c87126cd1ca06a7c5194c84940edd26d806
golovachevavp <golovacheva.vp@gmail.com>
2022-10-01 16:01:40 +0300

    2

--- src/main/java/ru/liga/ExchangeRate.java
+++ src/main/java/ru/liga/ExchangeRate.java
@@ -1,4 +1,5 @@
 package ru.liga;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -8,97 +9,109 @@ import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ExchangeRate {
 
 
     @SuppressWarnings("resource")
-    public static void main(String[] args) throws Exception {
-        for (; ;){
-            dialogBox(args);
-        }
-    }
-
-    /**
-     * Диалоговое окно с пользователем.
-     */
-    public static void dialogBox(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Прогнозирование курса валют. \n1. Валюта - евро");
-        System.out.println("2. Валюта - доллар США");
-        System.out.println("3. Валюта - турецкая лира");
-        System.out.print("Введите нужный Вам вариант: ");
-        int request1 = scan.nextInt();
-        int request2;
-        try {
-            switch (request1) {
-                case 1:
-                    System.out.println("1. Курс валюты на завтрашний день");
-                    System.out.println("2. Курс валюты на 7 дней");
-                    System.out.print("Введите нужный Вам вариант: ");
-                    request2 = scan.nextInt();
-                    switch (request2) {
-                        case 1:
-                            System.out.println("rate EUR tomorrow: " + rateTomorrow(
-                                    readerCSV("src/main/resources/EUR.csv")));
-                            break;
-                        case 2:
-                            System.out.println("rate EUR week: ");
-                            rateWeek(readerCSV("src/main/resources/EUR.csv"));
-                            break;
-                        default:
-                            System.out.println("Неверный запрос");
-                    }
-                    break;
+        boolean replay;
+        do {
+            replay = false;
+            System.out.println("Прогнозирование курса валют. \n1. Валюта - евро");
+            System.out.println("2. Валюта - доллар США");
+            System.out.println("3. Валюта - турецкая лира");
+            System.out.print("Введите нужный Вам вариант: ");
+            int request1 = scan.nextInt();
+            int request2;
+            try {
+                switch (request1) {
+                    case 1:
+                        do {
+                            replay = false;
+                            System.out.println("1. Курс валюты на завтрашний день");
+                            System.out.println("2. Курс валюты на 7 дней");
+                            System.out.print("Введите нужный Вам вариант: ");
+                            request2 = scan.nextInt();
+                            switch (request2) {
+                                case 1:
+                                    System.out.println("rate EUR tomorrow: " + rateTomorrow(
+                                            readerCSV("src/main/resources/EUR.csv")));
+                                    break;
+                                case 2:
+                                    System.out.println("rate EUR week: ");
+                                    rateWeek(readerCSV("src/main/resources/EUR.csv"));
+                                    break;
+                                default:
+                                    System.out.println("Неверный запрос");
+                                    replay = true;
+                            }
+                        } while (replay);
+                        break;
 
-                case 2:
-                    System.out.println("1. Курс валюты на завтрашний день");
-                    System.out.println("2. Курс валюты на 7 дней");
-                    System.out.print("Выберите необходимый вариант: ");
-                    request2 = scan.nextInt();
-                    switch (request2) {
-                        case 1:
-                            System.out.println("rate USD tomorrow: " + rateTomorrow(
-                                    readerCSV("src/main/resources/USD.csv")));
-                            break;
-                        case 2:
-                            System.out.println("rate USD week: ");
-                            rateWeek(readerCSV("src/main/resources/USD.csv"));
-                            break;
-                        default:
-                            System.out.println("Неверный запрос");
-                    }
-                    break;
+                    case 2:
+                        do {
+                            replay = false;
+                            System.out.println("1. Курс валюты на завтрашний день");
+                            System.out.println("2. Курс валюты на 7 дней");
+                            System.out.print("Выберите необходимый вариант: ");
+                            request2 = scan.nextInt();
+                            switch (request2) {
+                                case 1:
+                                    System.out.println("rate USD tomorrow: " + rateTomorrow(
+                                            readerCSV("src/main/resources/USD.csv")));
+                                    break;
+                                case 2:
+                                    System.out.println("rate USD week: ");
+                                    rateWeek(readerCSV("src/main/resources/USD.csv"));
+                                    break;
+                                default:
+                                    replay = true;
+                                    System.out.println("Неверный запрос");
+                            }
+                        } while (replay);
+                        break;
 
-                case 3:
-                    System.out.println("1. Курс валюты на завтрашний день");
-                    System.out.println("2. Курс валюты на 7 дней");
-                    System.out.print("Выберите необходимый вариант: ");
-                    request2 = scan.nextInt();
-                    switch (request2) {
-                        case 1:
-                            System.out.println("rate TRY tomorrow: " + rateTomorrow(
-                                    readerCSV("src/main/resources/TRY.csv")));
-                            break;
-                        case 2:
-                            System.out.println("rate TRY week: ");
-                            rateWeek(readerCSV("src/main/resources/TRY.csv"));
-                            break;
-                        default:
-                            System.out.println("Неверный запрос");
-                    }
-                    break;
-                default:
-                    System.out.println("Неверный запрос");
+                    case 3:
+                        do {
+                            replay = false;
+                            System.out.println("1. Курс валюты на завтрашний день");
+                            System.out.println("2. Курс валюты на 7 дней");
+                            System.out.print("Выберите необходимый вариант: ");
+                            request2 = scan.nextInt();
+                            switch (request2) {
+                                case 1:
+                                    System.out.println("rate TRY tomorrow: " + rateTomorrow(
+                                            readerCSV("src/main/resources/TRY.csv")));
+                                    break;
+                                case 2:
+                                    System.out.println("rate TRY week: ");
+                                    rateWeek(readerCSV("src/main/resources/TRY.csv"));
+                                    break;
+                                default:
+                                    replay = true;
+                                    System.out.println("Неверный запрос");
+                            }
+                        } while (replay);
+                        break;
+                    default:
+                        replay = true;
+                        System.out.println("Неверный запрос");
+                }
+            } catch (FileNotFoundException exc) {
+                System.out.println("Файл не найден");
             }
-        }catch (FileNotFoundException exc){
-            System.out.println("Файл не найден");
-        }
+        } while (replay);
     }
 
     /**
      * Извлекает из файла csv столбцы с датой и курсом и записывает их в ArrayList<DateAndCourse> course
+     *
      * @return ArrayList<DateAndCourse> course
      */
     public static List readerCSV(String csvFile) throws IOException {
@@ -107,28 +120,25 @@ public class ExchangeRate {
         List<String> courseString = new ArrayList<>();
         List<String> dateString = new ArrayList<>();
         List<DateAndCourse> course = new ArrayList<>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(csvFile));
-            while ((line = br.readLine()) != null) {
-                cols = line.split(";");
-                courseString.add((cols[2]));
-                dateString.add((cols[1]));
-            }
-            courseString.remove(0);
-            dateString.remove(0);
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
+        BufferedReader br = new BufferedReader(new FileReader(csvFile));
+        while ((line = br.readLine()) != null) {
+            cols = line.split(";");
+            courseString.add((cols[2]));
+            dateString.add((cols[1]));
+        }
+        courseString.remove(0);
+        dateString.remove(0);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("M/d/yyyy");
 
-            for (int i = 0, j = 0; i < courseString.size(); i++, j++) {
-                course.add(new DateAndCourse(Double.valueOf(courseString.get(i)), LocalDate.parse(dateString.get(j), formatter)));
-            }
-        }catch (IndexOutOfBoundsException exc){
-            System.out.println("Файл не найден");
+        for (int i = 0, j = 0; i < courseString.size(); i++, j++) {
+            course.add(new DateAndCourse(Double.valueOf(courseString.get(i)), LocalDate.parse(dateString.get(j), formatter)));
         }
         return course;
     }
 
     /**
      * Прогнозирует курс на завтрашний день.
+     *
      * @return возвращает день в формате d.MM.yyyy и курс в формате #.##
      */
     public static String rateTomorrow(List<DateAndCourse> course) {
@@ -170,7 +180,7 @@ public class ExchangeRate {
 
         Locale localeRu = new Locale("ru", "RU");
 
-        for(int i=6; i>=0; i--){
+        for (int i = 6; i >= 0; i--) {
             int year = course.get(i).date.getYear();
             int month = course.get(i).date.getMonthValue();
             int dayOfMonth = course.get(i).date.getDayOfMonth();
