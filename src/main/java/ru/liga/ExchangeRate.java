743ee0b28cf508b8c33fc20f2504f655d2b3bdc5
golovachevavp <golovacheva.vp@gmail.com>
2022-10-03 16:19:13 +0300

    2

--- src/main/java/ru/liga/ExchangeRate.java
+++ src/main/java/ru/liga/ExchangeRate.java
@@ -1,79 +1,71 @@
+
 package ru.liga;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.text.DecimalFormat;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.TextStyle;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Locale;
-import java.util.Scanner;
+        import java.io.BufferedReader;
+        import java.io.FileNotFoundException;
+        import java.io.FileReader;
+        import java.io.IOException;
+        import java.text.DecimalFormat;
+        import java.time.LocalDate;
+        import java.time.format.DateTimeFormatter;
+        import java.time.format.TextStyle;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Locale;
+        import java.util.Scanner;
 
 public class ExchangeRate {
 
 
     @SuppressWarnings("resource")
     public static void main(String[] args) throws IOException {
-        Scanner scan = new Scanner(System.in);
-        boolean replay;
-        do {
-            replay = false;
-            System.out.print("Прогнозирование курса валют. Введите запрос, по образцу:\"USD tomorrow или USD week.\" " +
-                    "\nВведите Ваш запрос- ");
-            String request = scan.nextLine();
-            try {
-                switch (request) {
-                    case "EUR tomorrow":
-                        System.out.println("rate EUR tomorrow: " + rateTomorrow(
-                                readerCSV("src/main/resources/EUR.csv")));
-                        break;
-                    case "USD tomorrow":
-                        System.out.println("rate USD tomorrow: " + rateTomorrow(
-                                readerCSV("src/main/resources/USD.csv")));
-                        break;
 
-                    case "TRY tomorrow":
-                        System.out.println("rate TRY tomorrow: " + rateTomorrow(
-                                readerCSV("src/main/resources/TRY.csv")));
-                        break;
-                    case "EUR week":
-                        System.out.println("rate EUR week: ");
-                        rateWeek(readerCSV("src/main/resources/EUR.csv"));
-                    case "USD week":
-                        System.out.println("rate USD week: ");
-                        rateWeek(readerCSV("src/main/resources/USD.csv"));
-                        break;
-                    case "TRY week":
-                        System.out.println("rate TRY week: ");
-                        rateWeek(readerCSV("src/main/resources/TRY.csv"));
-                        break;
-                    default:
-                        replay = true;
-                        System.out.println("Неверный запрос");
-                }
-            } catch (FileNotFoundException exc) {
-                System.out.println("Файл не найден");
-            }
-        } while (replay);
+        initConsole();
     }
+
+    public static void invoke(Scanner scan) throws IOException {
+        System.out.print("Прогнозирование курса валют. Введите запрос, по образцу:\"USD tomorrow или USD week.\" " +
+                "\nВведите Ваш запрос - ");
+        String request = scan.nextLine();
+        String currencyType;
+        if (request.contains("tomorrow")) {
+            currencyType = request.replaceAll("tomorrow", "");
+            currencyType = currencyType.replaceAll(" ", "");
+            rateTomorrow(readerCSV(currencyType), currencyType);
+        } else if (request.contains("week")) {
+            currencyType = request.replaceAll("week", "");
+            currencyType = currencyType.replaceAll(" ", "");
+            rateWeek(readerCSV(currencyType), currencyType);
+        } else {
+            throw new RuntimeException("Неправильный запрос");
+        }
+        initConsole();
+    }
+
+    public static void initConsole() {
+        Scanner scan = new Scanner(System.in);
+        try {
+            invoke(scan);
+        } catch (Exception e) {
+            //System.out.println(e.getMessage());
+            initConsole();
+        }
+    }
+
 
     /**
      * Извлекает из файла csv столбцы с датой и курсом и записывает их в ArrayList<DateAndCourse> course
      *
      * @return ArrayList<DateAndCourse> course
      */
-    public static List readerCSV(String csvFile) throws IOException {
+    public static List<DateAndCourse> readerCSV(String currencyType) throws IOException {
         String line;
         String[] cols;
         List<String> courseString = new ArrayList<>();
         List<String> dateString = new ArrayList<>();
         List<DateAndCourse> course = new ArrayList<>();
-        BufferedReader br = new BufferedReader(new FileReader(csvFile));
+        String csvFilePath = "src/main/resources/" + currencyType + ".csv";
+        BufferedReader br = new BufferedReader(new FileReader(csvFilePath));
         while ((line = br.readLine()) != null) {
             cols = line.split(";");
             courseString.add((cols[2]));
@@ -94,7 +86,7 @@ public class ExchangeRate {
      *
      * @return возвращает день в формате d.MM.yyyy и курс в формате #.##
      */
-    public static String rateTomorrow(List<DateAndCourse> course) {
+    public static void rateTomorrow(List<DateAndCourse> course, String currencyType) {
         LocalDate date = LocalDate.now().plusDays(1);
         DecimalFormat twoDForm = new DecimalFormat("#.##");
         while (!date.equals(course.get(0).date)) {
@@ -105,21 +97,14 @@ public class ExchangeRate {
             newCourse = newCourse / 7;
             course.add(0, new DateAndCourse(newCourse, course.get(0).date.plusDays(1)));
         }
-        int year = course.get(0).date.getYear();
-        int month = course.get(0).date.getMonthValue();
-        int dayOfMonth = course.get(0).date.getDayOfMonth();
-        DayOfWeek dayOfWeek = course.get(0).date.getDayOfWeek();
-        Locale localeRu = new Locale("ru", "RU");
-
-        return dayOfWeek.getDisplayName(TextStyle.SHORT, localeRu) +
-                " " + dayOfMonth + "." + month + "." + year + " - " + twoDForm.format(course.get(0).course);
+        System.out.println("rate " + currencyType + " tomorrow:" + localDateToString(course.get(0).date) + twoDForm.format(course.get(0).course));
     }
 
     /**
      * Прогнозирует курс на неделю.
      * Выводит дни в формате d.MM.yyyy и курс в формате #.##
      */
-    public static void rateWeek(List<DateAndCourse> course) {
+    public static void rateWeek(List<DateAndCourse> course, String currencyType) {
         LocalDate date = LocalDate.now().plusDays(7);
         DecimalFormat twoDForm = new DecimalFormat("#.##");
         while (!date.equals(course.get(0).date)) {
@@ -130,16 +115,17 @@ public class ExchangeRate {
             newCourse = newCourse / 7;
             course.add(0, new DateAndCourse(newCourse, course.get(0).date.plusDays(1)));
         }
-
-        Locale localeRu = new Locale("ru", "RU");
-
+        System.out.println("rate " + currencyType + " week:");
         for (int i = 6; i >= 0; i--) {
-            int year = course.get(i).date.getYear();
-            int month = course.get(i).date.getMonthValue();
-            int dayOfMonth = course.get(i).date.getDayOfMonth();
-            DayOfWeek dayOfWeek = course.get(i).date.getDayOfWeek();
-            System.out.println(dayOfWeek.getDisplayName(TextStyle.SHORT, localeRu) +
-                    " " + dayOfMonth + "." + month + "." + year + " - " + twoDForm.format(course.get(i).course));
+            System.out.println(localDateToString(course.get(i).date) + twoDForm.format(course.get(i).course));
         }
+    }
+
+    private static String localDateToString(LocalDate date) {
+        Locale localeRu = new Locale("ru", "RU");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LL yyyy");
+        String formattedString = date.format(formatter);
+        String dayOfWeek = date.getDayOfWeek().getDisplayName(TextStyle.SHORT, localeRu);
+        return dayOfWeek + " " + formattedString;
     }
 }
