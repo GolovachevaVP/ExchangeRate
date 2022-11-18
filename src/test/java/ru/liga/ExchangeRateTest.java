35f9e760dfb9dcdc6c8dd0386f1bb1b39e238bf0
golovachevavp <golovacheva.vp@gmail.com>
2022-11-18 12:29:30 +0300

    enum

--- src/test/java/ru/liga/ExchangeRateTest.java
+++ src/test/java/ru/liga/ExchangeRateTest.java
@@ -8,11 +8,6 @@ import java.io.IOException;
 import static ru.liga.ExchangeRate.invoke;
 
 public class ExchangeRateTest {
-//    @Test
-//    void checkingTheExecutionOfTheUserRequest() throws IOException {
-//        Assertions.assertEquals("пт 23.12.2022 - 25,28", invoke("rate TRY -day 23.12.2022 -alg linReg"));
-//    }
-
 
     @Test
     void checkingForAnIncorrectUserRequest() {
